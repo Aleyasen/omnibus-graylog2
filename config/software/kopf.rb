99919843aa900e281bd8e4bf09790e628097acc6
Amirhossein Aleyasen <aleyase2@illinois.edu>
2016-01-18 13:52:15 -0600

    Update MD5

--- config/software/kopf.rb
+++ config/software/kopf.rb
@@ -4,7 +4,7 @@ default_version "1.0"
 dependency "elasticsearch"
 
 source url: "https://github.com/lmenezes/elasticsearch-kopf/archive/1.0.zip",
-       md5: "73210c723911eaf7e92726dd869b8152"
+       md5: "d5829ecaba16ead444fe3a233f58be42"
 
 relative_path "elasticsearch-kopf-1.0"
 
