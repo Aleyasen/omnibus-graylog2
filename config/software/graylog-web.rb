ff89c8f7bbb2f3eb1e325d22af45f79ebf6ffe7f
Amirhossein Aleyasen <aleyase2@illinois.edu>
2016-01-19 13:13:18 -0600

    Update graylog-web.rb

--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -4,13 +4,9 @@ default_version project.build_version
 dependency "server-jre"
 dependency "runit"
 
-if version.include? '-beta' or version.include? '-rc'
-  source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-         md5: "6e393ab4ced367af3f51015b7a7be5ef"
-else
-  source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-         md5: "ffde989ec88cf41097a7083454d2d894"
-end
+source path: "/opt/repos/graylog2-web-interface/target/universal/graylog-web-interface-#{version}.tgz",
+    md5: "fcc0141e54f426d7e4c7e3e96a0cd9e9"
+
 
 relative_path "graylog-web-interface-#{version}"
 
